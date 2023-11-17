e32e2053dcc4fca045109f7a6df3f08f1348a9f7
Fatbobot <Fatbobot@gmail.com>
2023-11-17 16:30:11 -0400

    added user input, wasd

--- client.js
+++ client.js
@@ -1,3 +1,23 @@
+const { IP, PORT } = require("./constants");
+
 const net = require("net");
 
-const {connect} = require('./play');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host : IP, // IP address here,
+    port : PORT,  // PORT number here,
+  });
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write ("Name: P-J");
+  });
+  conn.setEncoding("utf8");
+  // interpret incoming data as text
+  return conn;
+};
+
+
+module.exports = {
+  connect,
+};
--- play.js
+++ play.js
@@ -1,39 +1,13 @@
+const {connect} = require('./client');
+const {setupInput} = require('./input');
+
+const { builtinModules } = require("module");
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost', // IP address here,
-    port: '50541',  // PORT number here,
-  });
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-    conn.write ("Name: P-J");
-  });
-  conn.setEncoding("utf8");
-  // interpret incoming data as text
-  return conn;
-};
-
+const conn = connect();
 console.log("Connecting ...");
-connect();
 
-module.exports = {
-  connect,
-};
+setupInput(conn);
 
 
 // setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function () {
-  // your code here
-};
